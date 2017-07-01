7d8ee79e871c3b127eef2dcf886de236e18f2af8
AugustusZ <augustus.z@outlook.com>
2017-06-30 20:51:38 -0700

    Enable adding and removing hashtags in "Hashtags" component
    Click tag to remove and click plus sign to add new hashtag.

--- app/components/hashtags/hashtags.component.ts
+++ app/components/hashtags/hashtags.component.ts
@@ -2,6 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'tw-hashtags',
-  templateUrl: './app/components/hashtags/hashtags.component.html'
+  templateUrl: './app/components/hashtags/hashtags.component.html',
+  styleUrls: ['./assets/css/hashtags.component.css']
 })
-export class HashtagsComponent {}
+export class HashtagsComponent {
+  tags: string[] = [];
+  isEditing: boolean;
+
+  constructor() {
+    this.tags.push('hashtag');
+    this.isEditing = false;
+  }
+  
+  onAddButtonClick(e) {
+    this.isEditing = !this.isEditing;
+  }
+
+  onTagClick(e, targetTag) {
+    console.debug(targetTag);
+    this.tags = this.tags.filter(tag => tag !== targetTag);
+    console.debug(this.tags);
+  }
+
+  addNewHashtag(newHashtag) {
+    this.tags.push(this.tagify(newHashtag));
+  }
+
+  tagify(str: string): string {
+    return str.replace(/[^A-Za-z0-9+]+/gi, '');
+  }
+}
